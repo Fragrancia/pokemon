11bfa5aa8f6fb9b4cfd69fa90bf7ad45bb8441a5
ARTHUR JOSÉ FRAGA <arthur_fraga@estudante.sesisenai.org.br>
2024-05-28 12:10:01 -0300

    poquedex

--- src/componets/Pokedex.jsx
+++ src/componets/Pokedex.jsx
@@ -1,41 +1,52 @@
-import { useState, useEffect } from "react"
 
-export default function Pokedex(){
-    const [id, setId] = useState(1);
-    const[pokemon, setPokemon] = useState(null)
+import { useState, useEffect } from "react";
 
-    const fetchData = async () => {//função assicronada para encontrar dados e conectar a API
-        try{
-            const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${id}`)
-            const data = await response.json();
-            setPokemon(data)
-        
-        }catch(error){
-            console.error('Error: ', error);
-        }
-        
+export default function Pokedex() {
+  const [id, setId] = useState(1); // ID inicial do Pokémon
+  const [pokemon, setPokemon] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = await response.json();
+      setPokemon(data);
+    } catch (error) {
+      console.error(error);
     }
-    useEffect(() => {
-        fetchData()
-    }, [id])
+  };
 
-    const nextPokemon = () =>{
-        setId(id + 1 )
+  useEffect(() => {
+    fetchData();
+  }, [id]);
+
+  const nextPokemon = () => {
+    setId(id + 1);
+  };
+
+  const prevPokemon = () => {
+    if (id > 1) {
+      setId(id - 1);
+    } else {
+      console.warn("Não é possível ir abaixo do Pokémon #1");
     }
+  };
 
-    return(
-        <div>
-            {pokemon &&(
-                <div className="pokemon">
-                     <h1>Pokemon</h1>
-                     <p>{pokemon.name}</p> 
-                     <p>Peso :{pokemon.weight}</p>
-                    <img src={pokemon.sprites.front_default} alt="Pokemon"/>
-                     <button onClick={nextPokemon}> Próximo</button>
-                </div>
-            
-            )}
-           
+  return (
+    <div>
+      {pokemon && (
+        <div className="pokemon">
+          <h1>Pokémon</h1>
+          <p>{pokemon.name}</p>
+          <p>Peso: {pokemon.weight}g</p>
+          <img src={pokemon.sprites.front_default} alt="Pokémon" />
+          <div className="buttons">
+            <button onClick={prevPokemon} className="previous">
+              Anterior
+            </button>
+            <button onClick={nextPokemon}>Próximo</button>
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
